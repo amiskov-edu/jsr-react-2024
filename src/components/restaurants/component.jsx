33dd5b4cb99cdde43caefd5c677fab94b2671086
Andrey Miskov <amiskov@ya.ru>
2024-05-20 22:41:19 +0300

    add conditional rendering
    - for restaurants
    - for reviews

--- src/components/restaurants/component.jsx
+++ src/components/restaurants/component.jsx
@@ -1,6 +1,10 @@
 import {Restaurant} from '../restaurant/component.jsx'
 
 export const Restaurants = ({restaurants}) => {
+    if (restaurants.length === 0) {
+        return <div>Restaurants not found.</div>
+    }
+
     return <>
         {restaurants.map(restaurant => <Restaurant restaurant={restaurant}/>)}
     </>
