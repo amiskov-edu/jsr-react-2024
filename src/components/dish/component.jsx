268a48ecb892b79809fb5e1822c234120b920f1c
Andrey Miskov <amiskov@ya.ru>
2024-05-20 22:34:46 +0300

    add dish counter

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,3 +1,13 @@
+import {useState} from 'react';
+
 export const Dish = ({dish}) => {
-    return <>{dish.name} - {dish.price} ({dish.ingredients.join(', ')})</>
+    const [count, setCount] = useState(0);
+    const inc = () => (count >= 5) ? setCount(5) : setCount(count + 1);
+    const dec = () => (count <= 0) ? setCount(0) : setCount(count - 1);
+
+    return <>{dish.name} - {dish.price} ({dish.ingredients.join(', ')})
+        <button onClick={dec}>-</button>
+        {count}
+        <button onClick={inc}>+</button>
+    </>
 }
