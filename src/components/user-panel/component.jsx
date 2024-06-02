1df612b02ab2f79923c9f5ddd8d5f402fe46ad78
Andrey Miskov <amiskov@ya.ru>
2024-06-02 08:17:00 +0300

    rename depending components

--- src/components/user-panel/component.jsx
+++ src/components/user-panel/component.jsx
@@ -1,6 +1,6 @@
 import {useUser, useSetUser} from "../../contexts/user-context.jsx";
-import {UserWelcome} from "./user-welcome.jsx";
-import {UserLogin} from "./user-login.jsx";
+import {UserWelcome} from "./_user-welcome.jsx";
+import {UserLogin} from "./_user-login.jsx";
 
 export function UserPanel() {
     const user = useUser();
