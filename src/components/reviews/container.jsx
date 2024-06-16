b3e737c28c8374de19f5aaea7379310d266fa029
Andrey Miskov <amiskov@ya.ru>
2024-06-16 10:16:17 +0300

    useRequest for reviews

--- src/components/reviews/container.jsx
+++ src/components/reviews/container.jsx
@@ -1,20 +1,21 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Reviews } from "./component.jsx";
-import { useEffect } from "react";
 import { getReviewsByRestaurantId } from "../../redux/entities/review/thunks/get-reviews-by-restaurant-id.js";
 import { selectRestaurantReviewIds } from "../../redux/entities/restaurant/selectors.js";
+import { useRequest } from "../../hooks/use-request.js";
+import { REQUEST_STATUS } from "../../redux/ui/request/constants.js";
+import { useMemo } from "react";
 
 export const ReviewsContainer = ({ restaurantId }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getReviewsByRestaurantId({ restaurantId }));
-  }, [dispatch, restaurantId]);
-
+  const requestParams = useMemo(() => ({ restaurantId }), [restaurantId]);
+  const requestStatus = useRequest(getReviewsByRestaurantId, requestParams);
   const reviewIds = useSelector((state) =>
     selectRestaurantReviewIds(state, restaurantId),
   );
 
+  if (requestStatus === REQUEST_STATUS.pending) {
+    return <div>Loading reviews...</div>;
+  }
   if (!reviewIds) {
     return;
   }
