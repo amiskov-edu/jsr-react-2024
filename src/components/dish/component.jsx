43829f28c516d5e5ecbc6a27f3c10aaf7f3d4246
Andrey Miskov <amiskov@ya.ru>
2024-06-16 07:04:18 +0300

    improve dish component

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -2,7 +2,7 @@ import { Counter } from "../counter/component.jsx";
 import { useUser } from "../../contexts/user-context.jsx";
 import { selectDishCount } from "../../redux/ui/cart/selectors.js";
 import { useDispatch, useSelector } from "react-redux";
-import { CartSlice } from "../../redux/ui/cart/index.js";
+import { increment, decrement } from "../../redux/ui/cart/index.js";
 
 const min = 0;
 const max = 7;
@@ -11,8 +11,8 @@ export const Dish = ({ id }) => {
   const user = useUser();
 
   const dispatch = useDispatch();
-  const increment = () => dispatch(CartSlice.actions.increment(id));
-  const decrement = () => dispatch(CartSlice.actions.decrement(id));
+  const handleIncrement = () => dispatch(increment(id));
+  const handleDecrement = () => dispatch(decrement(id));
 
   const count = useSelector((state) => selectDishCount(state, id));
   const dish = useSelector((state) => state.dish.entities[id]);
@@ -23,12 +23,12 @@ export const Dish = ({ id }) => {
   return (
     <>
       {dish.name} - {dish.price} ({dish.ingredients.join(", ")})
-      {true && (
+      {user && (
         <>
           <Counter
             count={count}
-            increment={increment}
-            decrement={decrement}
+            increment={handleIncrement}
+            decrement={handleDecrement}
             min={min}
             max={max}
           />
