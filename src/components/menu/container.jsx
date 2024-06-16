48f636e4ed8ad73f727c421707f70730f832d710
Andrey Miskov <amiskov@ya.ru>
2024-06-16 10:05:32 +0300

    add menu preloading state

--- src/components/menu/container.jsx
+++ src/components/menu/container.jsx
@@ -1,33 +1,26 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Menu } from "./component.jsx";
-import { useEffect, useState } from "react";
 import { getDishesByRestaurantId } from "../../redux/entities/dish/thunks/get-dishes-by-restaurant-id.js";
 import { selectRestaurantDishIds } from "../../redux/entities/restaurant/selectors.js";
-import { selectRequestStatus } from "../../redux/ui/request/selectors";
+import { useRequest } from "../../hooks/use-request.js";
+import { useMemo } from "react";
+import { REQUEST_STATUS } from "../../redux/ui/request/constants.js";
 
 export const MenuContainer = ({ restaurantId }) => {
-  const [request, setRequest] = useState();
-  const status = useSelector((state) =>
-    selectRequestStatus(state, request?.requestId),
+  // Без `useMemo` будет бесконечный перерендер, т.к. на каждый рендер будет
+  // создаваться новый объект, запускающий перерендер и по кругу.
+  const requestParams = useMemo(
+    () => ({ restaurantId, forceRefetch: true }),
+    [restaurantId],
   );
-  console.log(status);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const req = dispatch(
-      getDishesByRestaurantId({ restaurantId, forceRefetch: true }),
-    );
-    setRequest(req);
-    return () => {
-      // Чтобы заметить, нужно выставить таймаут > 0 в `simple_api/utils.js`
-      // и потыкать на разные табы. Вернутся данные только для последнего
-      // выбранного в таймауте ресторана, остальные запросы отменятся (`rejectd`).
-      req.abort("Aborted from useEffect return function.");
-    };
-  }, [dispatch, restaurantId]);
-
+  const requestStatus = useRequest(getDishesByRestaurantId, requestParams);
   const dishIds = useSelector((state) =>
     selectRestaurantDishIds(state, restaurantId),
   );
+
+  if (requestStatus === REQUEST_STATUS.pending) {
+    return <div>Loading menu...</div>;
+  }
   if (!dishIds) {
     return;
   }
