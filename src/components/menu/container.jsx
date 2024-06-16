0e083b002f50bef9fbd9918722c256dc33b4a682
Andrey Miskov <amiskov@ya.ru>
2024-06-16 09:44:55 +0300

    check menu status with request slice

--- src/components/menu/container.jsx
+++ src/components/menu/container.jsx
@@ -1,13 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Menu } from "./component.jsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getDishesByRestaurantId } from "../../redux/entities/dish/thunks/get-dishes-by-restaurant-id.js";
 import { selectRestaurantDishIds } from "../../redux/entities/restaurant/selectors.js";
+import { selectRequestStatus } from "../../redux/ui/request/selectors";
 
 export const MenuContainer = ({ restaurantId }) => {
+  const [request, setRequest] = useState();
+  const status = useSelector((state) =>
+    selectRequestStatus(state, request?.requestId),
+  );
+  console.log(status);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getDishesByRestaurantId({ restaurantId }));
+    const req = dispatch(
+      getDishesByRestaurantId({ restaurantId, forceRefetch: true }),
+    );
+    setRequest(req);
+    return () => {
+      // Чтобы заметить, нужно выставить таймаут > 0 в `simple_api/utils.js`
+      // и потыкать на разные табы. Вернутся данные только для последнего
+      // выбранного в таймауте ресторана, остальные запросы отменятся (`rejectd`).
+      req.abort("Aborted from useEffect return function.");
+    };
   }, [dispatch, restaurantId]);
 
   const dishIds = useSelector((state) =>
